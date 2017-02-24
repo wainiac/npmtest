3e9fb4a8f186fff6729fadf5bb469de2835cbb91
Wainiac <wainiac.dev@gmail.com>
2017-02-23 21:32:48 -0500

    Fixed jshint errors

--- tests/npmtest.js
+++ tests/npmtest.js
@@ -3,12 +3,12 @@ const npmtest = require('../index');
 
 describe("Test", () => {
     it("should be true", () => {
-        expect(true).to.be.true;
+        expect(true).to.be.equal(true);
     });
     it("should be false", () => {
-        expect(false).to.be.false;
+        expect(false).to.equal(false);
     });
     it("should be 'Test'", () => {
         expect(npmtest()).to.equal("Test");
-    })
+    });
 });
