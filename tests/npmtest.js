73c1fe03335050a8cd470ef6d840162a5c988283
Wayne Muller <wayne@bid.ventures>
2017-02-23 20:52:52 -0500

    Add test

--- tests/npmtest.js
+++ tests/npmtest.js
@@ -4,4 +4,7 @@ describe("Test", () => {
     it("should be true", () => {
         expect(true).to.be.true;
     });
+    it("should be false", () => {
+        expect(false).to.be.false;
+    });
 });
